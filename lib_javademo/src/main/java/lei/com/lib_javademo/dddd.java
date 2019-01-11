68eaff80653413466849f32a31540e1d2c5e7b73
18850415334 <858670589.lei>
2019-01-11 16:02:57 +0800

    first commmit

---
+++ lib_javademo/src/main/java/lei/com/lib_javademo/dddd.java
@@ -0,0 +1,8 @@
+package lei.com.lib_javademo;
+
+/**
+ * create by lei on 2019/1/11/011
+ * desc:
+ */
+public class dddd {
+}
---
+++ lib_javademo/src/main/java/lei/com/lib_javademo/synchronized_1/SynchronizedClass01.java
@@ -0,0 +1,8 @@
+package lei.com.lib_javademo.synchronized_1;
+
+/**
+ * create by lei on 2019/1/11/011
+ * desc:
+ */
+public class SynchronizedClass01 {
+}
